af0e79a09a88687ccad768c5a2a4e8791c968c23
Isen <3904043+isen-ng@users.noreply.github.com>
2019-09-03 19:16:45 +0800

    5.library book full tests (#5)

--- src/test/java/com/github/isenng/libraryunittesting/library/LibraryBookImplTests.java
+++ src/test/java/com/github/isenng/libraryunittesting/library/LibraryBookImplTests.java
@@ -2,6 +2,7 @@ package com.github.isenng.libraryunittesting.library;
 
 import com.github.isenng.libraryunittesting.library.duedates.DueDateCalculator;
 import com.github.isenng.libraryunittesting.library.exceptions.AlreadyCheckedOutException;
+import com.github.isenng.libraryunittesting.library.exceptions.NotCheckedOutException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,32 @@ class LibraryBookImplTests {
     }
 
     @Nested
+    class GetDueDate {
+        @Test
+        void ShouldThrowNotCheckedOutExceptionIfBookIsNotCheckedOut() {
+            // act
+            Throwable t = catchThrowable(() -> libraryBook.getDueDate());
+
+            // assert
+            assertThat(t).isInstanceOf(NotCheckedOutException.class);
+        }
+
+        @Test
+        void ShouldReturnDueDateIfBookIsCheckedOut() throws Exception {
+            // arrange
+            LocalDate expected = LocalDate.now();
+            when(mockDueDateCalculator.calculateDueDate()).thenReturn(expected);
+            libraryBook.checkOut();
+
+            // act
+            LocalDate result = libraryBook.getDueDate();
+
+            // assert
+            assertThat(result).isEqualTo(expected);
+        }
+    }
+
+    @Nested
     class CheckOut {
         @Test
         void ShouldThrowAlreadyCheckedOutExceptionIfBookIsCheckedOut() throws Exception {
@@ -61,6 +88,31 @@ class LibraryBookImplTests {
             // assert
             assertThat(result).isEqualTo(expected);
             verify(mockDueDateCalculator).calculateDueDate();
+        }
+    }
+
+    @Nested
+    class CheckIn {
+        @Test
+        void ShouldThrowNotCheckedOutExceptionIfBookIsCheckedOut() throws Exception {
+            // act
+            Throwable t = catchThrowable(() -> libraryBook.checkIn());
+
+            // assert
+            assertThat(t).isInstanceOf(NotCheckedOutException.class);
+        }
+
+        @Test
+        void ShouldSucceedIfBookIsCheckedOut() throws Exception {
+            // arrange
+            when(mockDueDateCalculator.calculateDueDate()).thenReturn(LocalDate.now());
+            libraryBook.checkOut();
+
+            // act
+            Throwable t = catchThrowable(() -> libraryBook.checkIn());
+
+            // assert
+            assertThat(t).isNull();
         }
     }
 }
