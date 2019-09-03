a9eedbaaf89a4e27a81cff10bb0b849bbc315f65
Isen <3904043+isen-ng@users.noreply.github.com>
2019-09-03 19:14:59 +0800

    4.library book full implementation (#4)

--- src/main/java/com/github/isenng/libraryunittesting/library/LibraryBook.java
+++ src/main/java/com/github/isenng/libraryunittesting/library/LibraryBook.java
@@ -1,6 +1,7 @@
 package com.github.isenng.libraryunittesting.library;
 
 import com.github.isenng.libraryunittesting.library.exceptions.AlreadyCheckedOutException;
+import com.github.isenng.libraryunittesting.library.exceptions.NotCheckedOutException;
 
 import java.time.LocalDate;
 
@@ -9,9 +10,22 @@ import java.time.LocalDate;
  */
 public interface LibraryBook {
     /**
+     * Gets the due date of this book
+     * @return the due date of this book
+     * @throws NotCheckedOutException when this book is not checked out
+     */
+    LocalDate getDueDate() throws NotCheckedOutException;
+
+    /**
      * Checks out a book
      * @return the due date
      * @throws AlreadyCheckedOutException when this book is already checked out
      */
     LocalDate checkOut() throws AlreadyCheckedOutException;
+
+    /**
+     * Returns a book
+     * @throws NotCheckedOutException when this book is not checked out
+     */
+    void checkIn() throws NotCheckedOutException;
 }
